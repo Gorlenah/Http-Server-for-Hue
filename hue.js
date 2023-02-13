a19ed595b8487139aaae943edc07603887ded6b1
Facchinetti Stefano <34067164+Gorlenah@users.noreply.github.com>
2023-02-13 17:22:03 +0100

    code reorganisation, light status automation
    now at each request the current state of the lights is checked via hue bridge, consequently the next state is changed, if the states of the two lights do not match, the choice is to switch them off by default, improved code and placed at the beginning the server parameters

--- hue.js
+++ hue.js
@@ -1,111 +1,119 @@
 /* Notes by Gorlenah
 N.B: This is a test file, used in a safe enviroment, no consideration 
-was given to the security of the code for use outside the LAN, moreover, 
-the code is not optimised because I wanted to test operation 
-between a shelly relay / browser and the hue bridge first
+was given to the security of the code for use outside the LAN
 
 HUE API : https://developers.meethue.com/develop/get-started-2/
+
+INSTRUCTIONS / GIT: https://github.com/Gorlenah/Http-Server-for-Hue
 */
+
+//PARAMETERS
 const http = require('http');
-
-const hostname = 'SERVER-IP';
-const port = 3000;
-//Stateful Variable, is the state of light by hue api (philips hue api)
-var state = true;
-
-const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  
 //hue require https
 const https = require('https');
+//hue requires your Api Key
+const hueApiKey = '/api/HUE-API-KEY';
+//hue bridge IP
+const bridgeIp = 'HUE-BRIDGE-IP';
+//hue bridge port
+const bridgePort = 443;
+//server IP
+const hostname = 'SERVER-IP';
+//server port
+const port = 3000;
+//response title, used in console and http response
+const title = 'Philips Hue Change Status Server made by Gorlenah\n';
 
-var jsonObject = {
-    "on" : state
-};
-var putData = JSON.stringify(jsonObject);
-
-var optionsLight1 = {
-  hostname: 'HUE-BRIDGE-IP',
-  port: 443,
-  //api key is an unique generated key by hue bridge (see philips hue api)
-  path: '/api/HUE-API-KEY/lights/1/state',
-  method: 'PUT',
-  headers: {
-       'Content-Type': 'application/json',
-       'Content-Length': putData.length
-     }
-};
-
-//Skip TLS CA verification, hue have an self signed CA
+//Skip TLS CA verification, hue bridge have an self signed CA
 process.env["NODE_TLS_REJECT_UNAUTHORIZED"] = 0;
 
-var reqLight1 = https.request(optionsLight1, (res) => {
-  console.log('statusCode:', res.statusCode);
-  console.log('headers:', res.headers);
+/* With this function you ask the bridge the boolean status of the light, true = on,
+This function will return the light boolean state
+var lightNumber -> is the light number in hue bridge
+*/
+async function lightStatus(lightNumber) {
+    const url = 'https://'.concat(bridgeIp).concat(hueApiKey).concat(/lights/).concat(lightNumber);
+    const response = await fetch(url);
+    
+    if (await !response.ok) {
+      throw new Error(`HTTP error ${response.status}`);
+    }
+    
+    const light = await response.json();
+    return light.state.on;
+}
 
-  res.on('data', (d) => {
-    process.stdout.write(d);
-  });
-});
+/* With this function you create the HTTP PUT for the bridge
+var lightNumber -> is the light number in hue bridge
+var putData -> is the Json Data for the Light Status
+*/
+function optionsLight(lightNumber, putData){
+  return options = {
+    hostname: bridgeIp,
+    port: bridgePort,
+    path: hueApiKey.concat('/lights/').concat(lightNumber).concat('/state'),
+    method: 'PUT',
+    headers: {
+      'Content-Type': 'application/json',
+      'Content-Length': putData.length
+    }
+  };
+}
 
-reqLight1.on('error', (e) => {
-  console.error(e);
-});
+//Return Current Date formatted with UTC
+function consoleDate(){
+  var date = new Date(Date.now());
+  return date.toUTCString()
+}
 
-reqLight1.write(putData);
-reqLight1.end();
+const server = http.createServer(async (req, res) => {
 
-//Same with the second light or more
-var optionsLight2 = {
-  hostname: 'HUE-BRIDGE-IP',
-  port: 443,
-  path: '/api/HUE-API-KEY/lights/2/state',
-  method: 'PUT',
-  headers: {
-       'Content-Type': 'application/json',
-       'Content-Length': putData.length
-     }
-};
-
-var reqLight2 = https.request(optionsLight2, (res) => {
-  console.log('statusCode:', res.statusCode);
-  console.log('headers:', res.headers);
-
-  res.on('data', (d) => {
-    process.stdout.write(d);
-  });
-});
-
-reqLight2.on('error', (e) => {
-  console.error(e);
-});
-
-reqLight2.write(putData);
-reqLight2.end();
-
-
-var temp = state;
-//Change boolean to turn off light next time (Can improve by reading current state from Hue Bridge)
-state = !state;
-
-res.end('State sent = ' + temp + '\n' + 'Next State = ' + state + '\n');
+try {
   
+  //Get lights current status
+  var status1 = await lightStatus(1);
+  var status2 = await lightStatus(2);
+  var oldStatus;
+  
+  //Turn off lights if they have different status, or change status
+  if(status1 === status2){
+    var jsonObject = {
+      "on" : !status1
+    };
+    oldStatus = status1;
+  } else {
+    var jsonObject = {
+      "on" : false
+    };
+    oldStatus = true;
+  }
+  
+  var putData = JSON.stringify(jsonObject);
+
+  var reqLight1 = https.request(optionsLight(1, putData));
+  reqLight1.write(putData);
+  reqLight1.end();
+
+  var reqLight2 = https.request(optionsLight(2, putData));
+  reqLight2.write(putData);
+  reqLight2.end();
+  
+  console.log(consoleDate()+' | old status: '+oldStatus+' | new status: '+!oldStatus+' |');
+  
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
+  res.end(title+'\n old status: '+oldStatus+'\n new status: '+!oldStatus+'\n');
+  
+} catch (error) {
+
+  console.log(consoleDate() + error);
+  res.statusCode = 500;
+  res.end(title + 'Server Error 500 - See Console\n');
+}
 });
 
 //Console reminder for hostname and port used
 server.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}/`);
+  console.log(title + `Server running at http://${hostname}:${port}/`);
 });
 
-/*
-P.S:
-To check PID port utilization use :
-lsof -i :3000
-will return a list of PID using that port on linux
-
-kill -9 (PID)
-to kill that process and free your Server Port
-
-Use cmd+C to close the process, so you don't have to kill it by PID
-*/
