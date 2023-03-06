16e9dbd60491da77a61b9fc83dd1cde3980d9edf
Facchinetti Stefano <34067164+Gorlenah@users.noreply.github.com>
2023-03-06 15:32:27 +0100

    Add Persistance Examples

---
+++ persistance-example/startServerHue.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+nodemon /home/fc/Downloads/Http-Server-for-Hue-main/hue.js > /home/fc/Downloads/Http-Server-for-Hue-main/logHue.txt
---
+++ persistance-example/startServerHueDim.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+nodemon /home/fc/Downloads/Http-Server-for-Hue-main/hue-dim.js > /home/fc/Downloads/Http-Server-for-Hue-main/logHueDim.txt
